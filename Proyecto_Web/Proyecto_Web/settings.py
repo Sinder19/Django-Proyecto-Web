510a750829632621d3766b471e43c81b1945da81
Sinder19 <es.machado@duocuc.cl>
2021-07-01 02:45:25 -0400

    ignorar

--- Proyecto_Web/Proyecto_Web/settings.py
+++ Proyecto_Web/Proyecto_Web/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Proyecto_Web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': '127.0.0.1:1521/xe',
+        'NAME': '127.0.0.1:1521/orcl',
         'USER': 'AmonStore',
         'PASSWORD': 'amonstore123',
         'TEST': {
